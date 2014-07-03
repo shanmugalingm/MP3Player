05b33534e5acb8e41e57d9e110c80661fdb3367e
shanmugalingm <shano.shanugalingm@gmail.com>
2014-07-03 17:43:04 +1000

    MP3 Payer.
    This project contains code related to playing MP3 Files in java.  It
    uses JZoom.  This player could loop an MP3 till it is stopped by the
    user.
    
    This is built as a Maven Project.

---
+++ MP3Player/src/main/java/com/player/mp3/RawPlayer.java
@@ -0,0 +1,69 @@
+package com.player.mp3;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.SourceDataLine;
+
+public class RawPlayer {
+	
+	public void testPlay(InputStream stream)
+	{
+	  try {
+	    AudioInputStream in= AudioSystem.getAudioInputStream(stream);
+	    AudioInputStream din = null;
+	    AudioFormat baseFormat = in.getFormat();
+	    AudioFormat decodedFormat = new AudioFormat(AudioFormat.Encoding.PCM_SIGNED, 
+	                                                                                  baseFormat.getSampleRate(),
+	                                                                                  16,
+	                                                                                  baseFormat.getChannels(),
+	                                                                                  baseFormat.getChannels() * 2,
+	                                                                                  baseFormat.getSampleRate(),
+	                                                                                  false);
+	    din = AudioSystem.getAudioInputStream(decodedFormat, in);
+	    // Play now. 
+	    rawplay(decodedFormat, din);
+	    in.close();
+	  } catch (Exception e)
+	    {
+	        //Handle exception.
+	    }
+	} 
+
+	private void rawplay(AudioFormat targetFormat, AudioInputStream din) throws IOException,                                                                                                LineUnavailableException
+	{
+	  byte[] data = new byte[4096];
+	  SourceDataLine line = getLine(targetFormat); 
+	  if (line != null)
+	  {
+	    // Start
+	    line.start();
+	    int nBytesRead = 0, nBytesWritten = 0;
+	    while (nBytesRead != -1)
+	    {
+	        nBytesRead = din.read(data, 0, data.length);
+	        if (nBytesRead != -1) nBytesWritten = line.write(data, 0, nBytesRead);
+	    }
+	    // Stop
+	    line.drain();
+	    line.stop();
+	    line.close();
+	    din.close();
+	  } 
+	}
+
+	private SourceDataLine getLine(AudioFormat audioFormat) throws LineUnavailableException
+	{
+	  SourceDataLine res = null;
+	  DataLine.Info info = new DataLine.Info(SourceDataLine.class, audioFormat);
+	  res = (SourceDataLine) AudioSystem.getLine(info);
+	  res.open(audioFormat);
+	  return res;
+	} 
+
+}
